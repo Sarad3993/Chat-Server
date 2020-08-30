e11b2937d597926184c03a436a294c5d9679757f
Sarad3993 <risingstar3993@gmail.com>
2020-08-30 18:03:52 +0545

    implemented channel layer to broadcast messages

--- ProjectX/settings.py
+++ ProjectX/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = '/static/'
 
 # channels
 ASGI_APPLICATION = 'ProjectX.routing.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
